d450d5d4e5d77831fdebb81412a58b1e3fe1858a
MrPancetita <contreras.engineer@outlook.com>
2025-04-02 10:46:33 +0200

    improved for spanish

--- pruebaBatch.py
+++ pruebaBatch.py
@@ -2,7 +2,7 @@ import uuid
 import requests
 import json
 
-# Este es el que hay que usar para, a partir del texto de noticia.txt generar el archivo de audio por batch synthesis
+# Este es el que hay que usar para, a partir del texto de texto_input.txt generar el archivo de audio por batch synthesis
 
 synthesis_id = uuid.uuid4()
 print(f"Synthesis ID: {synthesis_id}")
@@ -12,9 +12,9 @@ with open('config.json', 'r') as config_file:
 
 subscription_key = config.get("SubscriptionKey")
 
-# Leer el contenido de noticia.txt
-with open('noticia.txt', 'r', encoding='utf-8') as noticia_file:
-    noticia_content = noticia_file.read()
+# Leer el contenido de texto_input.txt
+with open('texto_input.txt', 'r', encoding='utf-8') as texto_input_file:
+    texto_input_content = texto_input_file.read()
 
 url = f"https://eastus2.api.cognitive.microsoft.com/texttospeech/batchsyntheses/{synthesis_id}?api-version=2024-04-01"
 headers = {
@@ -22,11 +22,11 @@ headers = {
     "Content-Type": "application/json"
 }
 data = {
-    "description": "My first ssml test",
+    "description": "My first ssml conversor",
     "inputKind": "SSML",
     "inputs": [
         {
-            "content": f"<speak version=\"1.0\" xml:lang=\"en-US\"><voice name=\"es-ES-TristanMultilingualNeural\">{noticia_content}</voice></speak>"
+            "content": f"<speak version=\"1.0\" xml:lang=\"es-ES\"><voice name=\"es-ES-TristanMultilingualNeural\">{texto_input_content}</voice></speak>"
         }
     ],
     "properties": {
